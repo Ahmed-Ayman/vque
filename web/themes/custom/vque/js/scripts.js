5b33c731365f0511895b7476e749a0f6b09cce66
Ahmed Ayman <ahmedayman055g@gmail.com>
2021-11-07 19:32:51 +0200

    style the screens

--- web/themes/custom/vque/js/scripts.js
+++ web/themes/custom/vque/js/scripts.js
@@ -6,5 +6,11 @@
   $(document).ready(function () {
     // Add code here, like
     // alert("Welcome to the web site!");
+    let counter = document.querySelector(".field.field--name-field-counter.field--type-string.field--label-above .field__item");
+    let cValue = counter.innerHTML;
+
+    counter.innerHTML = "";
+    $(".field__item").sevenSeg({ value: cValue });
+
   });
 })(jQuery, Drupal, this, this.document);
